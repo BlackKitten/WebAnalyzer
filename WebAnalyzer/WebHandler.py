1e221217be91a2b9430295a87ecdc6260172deb7
BlackKitten <b.public.griffin@gmail.com>
2015-10-28 11:40:33 +0100

    project setup

---
+++ WebAnalyzer/WebHandler.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 28, 2015
+
+@author: BlackKitten
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
